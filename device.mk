e4a5771322179536e9ba8a62dbdd38525134ab51
Tri <you@example.com>
2021-07-25 19:01:39 +0300

    Add init.pxa988.*.rc files

--- device.mk
+++ device.mk
@@ -32,77 +32,85 @@ PRODUCT_PACKAGES += \
     $(LOCAL_PATH)/audio/codec_pga.xml \
     $(LOCAL_PATH)/audio/tiny_hw.xml
 
-# 
-# # Camera
-# PRODUCT_PACKAGES += \
-#     camera.pxa988
-# 
-# # Codecs
-# PRODUCT_PACKAGES += \
-#     libcolorformat_switcher \
-#     libstagefrighthw \
-#     libstagefright_sprd_aacdec \
-#     libstagefright_sprd_mp3dec \
-#     libstagefright_sprd_h264dec \
-#     libstagefright_sprd_h264enc \
-#     libstagefright_sprd_mpeg4dec \
-#     libstagefright_sprd_mpeg4enc \
-#     libstagefright_sprd_vpxdec
-#
+# TODO
+# Camera
+PRODUCT_PACKAGES += \
+    camera.pxa988
+
+# TODO
+# Codecs
+PRODUCT_PACKAGES += \
+    libcolorformat_switcher \
+    libstagefrighthw \
+    libstagefright_sprd_aacdec \
+    libstagefright_sprd_mp3dec \
+    libstagefright_sprd_h264dec \
+    libstagefright_sprd_h264enc \
+    libstagefright_sprd_mpeg4dec \
+    libstagefright_sprd_mpeg4enc \
+    libstagefright_sprd_vpxdec
+
+# TODO
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/sirfgps.conf:system/etc/sirfgps.conf \
 	$(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/gps.xml:system/etc/gps.xml \
-# 
-# # HWC
-# PRODUCT_PACKAGES += \
-#     gralloc.pxa988 \
-#     hwcomposer.pxa988 \
-#     sprd_gsp.pxa988 \
-#     libmemoryheapion \
-#     libion_sprd
-# 
+
+# TODO
+# HWC
+PRODUCT_PACKAGES += \
+    gralloc.pxa988 \
+    hwcomposer.pxa988 \
+    sprd_gsp.pxa988 \
+    libmemoryheapion \
+    libion_sprd
+
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/sci-keypad.kl:system/usr/keylayout/sci-keypad.kl \
     $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
-# 
-# # Lights
-# PRODUCT_PACKAGES += \
-#     lights.pxa988
-# 
-# # Media config
-# MEDIA_XML_CONFIGS := \
-#     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml \
-#     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml
-# PRODUCT_COPY_FILES += \
-#     $(foreach f,$(MEDIA_XML_CONFIGS),$(f):system/etc/$(notdir $(f)))
-# PRODUCT_PACKAGES += \
-#     media_codecs.xml \
-#     media_profiles.xml
-# 
-# # Modules
-# PRODUCT_PACKAGES += \
-#     autotst.ko \
-#     mali.ko \
-#     mmc_test.ko \
-#     sprdwl.ko
-# 
-# # PowerHAL
-# PRODUCT_PACKAGES += \
-#     power.pxa988
-# 
-# # Ramdisk
-# PRODUCT_PACKAGES += \
-#     fstab.pxa988 \
-#     init.board.rc \
-#     init.pxa988.rc \
-#     init.pxa988.usb.rc \
-#     init.storage.rc \
-#     init.wifi.rc \
-#     ueventd.pxa988.rc
-# 
+
+# TODO
+# Lights
+PRODUCT_PACKAGES += \
+    lights.pxa988
+
+# TODO
+# Media config
+MEDIA_XML_CONFIGS := \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml
+PRODUCT_COPY_FILES += \
+    $(foreach f,$(MEDIA_XML_CONFIGS),$(f):system/etc/$(notdir $(f)))
+PRODUCT_PACKAGES += \
+    media_codecs.xml \
+    media_profiles.xml
+
+# TODO
+# Modules
+PRODUCT_PACKAGES += \
+    autotst.ko \
+    mali.ko \
+    mmc_test.ko \
+    sprdwl.ko
+
+# TODO
+# PowerHAL
+PRODUCT_PACKAGES += \
+    power.pxa988
+
+# TODO
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.pxa988 \
+    init.board.rc \
+    init.pxa988.rc \
+    init.pxa988.usb.rc \
+    init.storage.rc \
+    init.wifi.rc \
+    ueventd.pxa988.rc
+
 # WiFi
 PRODUCT_PACKAGES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf \
