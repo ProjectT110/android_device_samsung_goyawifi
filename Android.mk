da4fa28892b8b7dcc15f3ecaab3914fd3ea356a1
Tri <you@example.com>
2021-07-26 00:06:48 +0300

    Fix Bluetooth

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),goyawifi)
+bluetooth_C_INCLUDES := $(BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR)
+bluetooth_CFLAGS += -DHAS_BDROID_BUILDCFG
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
